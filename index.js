655fffa66b50fd35e589b4ad5ea7d98e98fd41f2
SojibMir27 <mdsojibmir2027@gmail.com>
2025-12-28 19:40:23 +0600

    added all product get api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,19 +15,35 @@ const client = new MongoClient(uri, {
 });
 
 app.use(cors());
-app.use(express.json());
+
+// app.use(express.json());
 
 async function run() {
   try {
     await client.connect();
     // start api create \\
     const db = client.db("simpleDB");
-    const productsCollectons = db.collection("products");
+    const productsCollection = db.collection("products");
 
+    // all product get api
     app.get("/products", async (req, res) => {
-      const result = await productsCollectons.find().toArray();
+      const result = await productsCollection.find().toArray();
       res.send(result);
     });
+
+    // app.post("/products", async (req, res) => {
+    //   const productData = req.body;
+
+    //   const result = await productsCollection.insertOne(productData);
+    //   res.send(result);
+    // });
+
+    // app.delete("/products", async (req, res) => {
+    //   const id = req.params.id;
+    //   const objectId = { _id: new ObjectId(id) };
+    //   const result = await productsCollection.deleteOne(objectId);
+    //   res.send(result);
+    // });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
