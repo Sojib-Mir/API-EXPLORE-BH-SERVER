20b3e2daba58d21efe2ebfb13d2e981f3eaa8734
SojibMir27 <mdsojibmir2027@gmail.com>
2025-12-31 19:30:26 +0600

    added some apis

--- index.js
+++ index.js
@@ -31,12 +31,41 @@ async function run() {
       res.send(result);
     });
 
+    // recent product get api
+    app.get("/recent-products", async (req, res) => {
+      const result = await productsCollection
+        .find()
+        .sort({ createdAt: -1 })
+        .limit(8)
+        .toArray();
+      res.send(result);
+    });
+
+    // manager email get all data
+    app.get("/my-products/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log("Manager Email==========>", email);
+
+      const query = { managerEmail: email };
+
+      console.log(query);
+
+      const result = await productsCollection
+        .find(query)
+        .sort({ createdAt: -1 })
+        .toArray();
+
+      res.send(result);
+    });
+
     // single data product get api
     app.get("/products/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id);
+
+      console.log("chele id=========>", id);
 
       const query = { _id: new ObjectId(id) };
+      console.log("meye id========>", query);
 
       const result = await productsCollection.findOne(query);
       console.log(result);
@@ -46,16 +75,44 @@ async function run() {
     // create product api
     app.post("/products", async (req, res) => {
       const productData = req.body;
+
       const result = await productsCollection.insertOne(productData);
       res.send(result);
     });
 
-    // app.delete("/products", async (req, res) => {
-    //   const id = req.params.id;
-    //   const objectId = { _id: new ObjectId(id) };
-    //   const result = await productsCollection.deleteOne(objectId);
-    //   res.send(result);
-    // });
+    // single product delete
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // all product delete
+    app.delete("/products", async (req, res) => {
+      const result = await productsCollection.deleteMany();
+      res.send(result);
+    });
+
+    // Product Update API (PATCH)
+    app.patch("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateData = req.body;
+
+      const filter = { _id: new ObjectId(id) };
+
+      const updatedDoc = {
+        $set: {
+          name: updateData.name,
+          price: updateData.price,
+          category: updateData.category,
+          image: updateData.image,
+        },
+      };
+
+      const result = await productsCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
